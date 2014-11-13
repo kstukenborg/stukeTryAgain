b36754992b443846938fa19e660375bfd20cc579
kathleen <kathleen@10.0.0.25>
2014-11-13 14:30:21 -0800

    first commit

--- src/DemoGit.java
+++ src/DemoGit.java
@@ -4,9 +4,6 @@ public class DemoGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git!");
-		System.out.println("Hello Git!");
-		System.out.println("Hello Git!");
-		
 	}
 
 }
